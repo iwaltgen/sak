e92d9b5090d47a7224d49f717c1d72b0bede21c1
iwaltgen <iwaltgen@gmail.com>
2022-09-29 01:52:27 +0900

    update: add CLI metadata

--- cmd/sak/main.go
+++ cmd/sak/main.go
@@ -7,12 +7,30 @@ import (
 	"github.com/urfave/cli/v2"
 
 	"github.com/iwaltgen/sak/cmd/sak/rand"
+	"github.com/iwaltgen/sak/internal"
 )
 
 func main() {
 	app := &cli.App{
-		Name:  "sak",
-		Usage: "multi-tool cli",
+		Name:     "sak",
+		Usage:    "Multi-tool CLI",
+		Version:  internal.Version(),
+		Compiled: internal.BuildDate(),
+		Authors: []*cli.Author{
+			{
+				Name:  "iwaltgen",
+				Email: "iwaltgen@gmail.com",
+			},
+		},
+
+		Suggest:                true,
+		UseShortOptionHandling: true,
+		EnableBashCompletion:   true,
+
+		Before: func(ctx *cli.Context) error {
+			log.SetFlags(0)
+			return nil
+		},
 		Commands: []*cli.Command{
 			rand.Cmd,
 		},
